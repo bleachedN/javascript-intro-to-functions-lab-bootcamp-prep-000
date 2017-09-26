67c261e6a724f2caa0ec9b1cb3f5c3a82b3de85f
Tracy Fernandez <tr.ferna@gmail.com>
2017-09-26 02:15:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function sayHiToGrandma(string) {
-  console.log("YES INDEED!")
-  return string. toUpperCase
+function logShout(string) {
+  console.log(string)
+  return string.toUpperCase
 }
