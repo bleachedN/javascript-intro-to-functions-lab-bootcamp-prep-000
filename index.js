be2827fb3f83ab812e4bfefee30ae2a6909bbec0
Tracy Fernandez <tr.ferna@gmail.com>
2017-09-26 02:19:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {
-  console.log(logshout)
-  return string.toUpperCase
+  console.log()
+  return strong.toUpperCase
 }
