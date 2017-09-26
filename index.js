50806849c03038b5f0c96e2a1efd6c78370c4852
Tracy Fernandez <tr.ferna@gmail.com>
2017-09-26 02:06:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function sayHiToGrandma(string) {
-  return string.toUpperCase()
+function logshout(string) {
+  console.log(toUpperCase)
 }
